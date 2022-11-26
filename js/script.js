ae22c80f89ee31aa137a700a7c64ff741fbb6340
Martin <martindrus@outlook.de>
2022-11-26 15:28:53 +0100

    styled

--- js/script.js
+++ js/script.js
@@ -4,11 +4,14 @@ const container = document.querySelector('#autocomplete-container');
 const autocomplete = container.querySelector('#autocomplete');
 const mainInput = container.querySelector('#main-input');
 
-let outputContainer = document.querySelector("#found-pokemon-container")
+let outputContainer = document.querySelector("#found-pokemon-container");
+let resultContainer = document.querySelector("#result-container");
 
 function getNames(language_id) {
     return names.filter(pokemon => (pokemon.local_language_id == language_id || pokemon.local_language_id == 9))
 }
+
+
 
 let getGermanNames = getNames(6)
 
@@ -52,7 +55,7 @@ function onKeyUp(e) {
     if (foundPokemon.id !== undefined) {
         let pokemonId = `pokemon/${foundPokemon.id}`
 
-        renderCard(pokemonId);
+        renderCard(pokemonId, foundPokemon.name);
     }
 }
 
@@ -62,28 +65,9 @@ function keyChecker(e, key) {
         'Enter':13,
         'ArrowLeft':39
     }
-  
     if (e.keyCode === keys[key] || e.which === keys[key] || e.key === key) return true;
     return false;
 }
-
-
-
-
-// function fetchData(inquiry) {
-
-//     let pokemonInformation = null;
-
-//     fetch(`https://pokeapi.co/api/v2/${inquiry}`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//         pokemonInformation = data;
-//         console.log(pokemonInformation);
-//         return pokemonInformation;
-//     })
-//     .catch(error => console.log(error));
-
-// }
 
 const fetchData = async (inquiry) => {
     let response = await fetch(`https://pokeapi.co/api/v2/${inquiry}`)
@@ -91,44 +75,127 @@ const fetchData = async (inquiry) => {
     return data;
 }
 
-
-
-
-async function renderCard(id) {
+async function renderCard(id, translatedName) {
     outputContainer.replaceChildren();
 
     let pokemonData = await fetchData(id);
 
     let image = document.createElement("img");
     image.src = pokemonData.sprites.front_default;
-
     outputContainer.appendChild(image);
 
+    let nameContainer = document.createElement("div");
+    nameContainer.id = "name-container"
+    nameContainer.innerText = `${firstCharToUpperCase(pokemonData.name)} \n ${firstCharToUpperCase(translatedName)}`
+    outputContainer.appendChild(nameContainer)
+
+    let typeContainer = document.createElement("div");
     let types = pokemonData.types;
-    let firstType = types[0].type
+
+    let firstType = types[0].type;
+    let firstTypeContainer = document.createElement("div");
+    firstTypeContainer.classList.add("own-type-container")
+    firstTypeContainer.style.backgroundColor = getTypeColor(firstType.name)
+    firstTypeContainer.textContent = firstType.name;
+    typeContainer.appendChild(firstTypeContainer);
+
     let secondType = null;
     if (types.length === 2) {
         secondType = types[1].type
+        let secondTypeContainer = document.createElement("div");
+        secondTypeContainer.classList.add("own-type-container")
+        secondTypeContainer.style.backgroundColor = getTypeColor(secondType.name)
+        secondTypeContainer.textContent = secondType.name;
+        typeContainer.appendChild(secondTypeContainer);
     }
 
+    outputContainer.appendChild(typeContainer);
 
     let typeEffectiveness = await getDamageRelations(firstType, secondType);
     let relevantEffectiveness = deleteAveragedEffectiveness(typeEffectiveness);
+    let myMap = separateRelevantEffectiveness(relevantEffectiveness)
+    let sortedMap = new Map([...myMap.entries()].sort().reverse());
 
-    console.log(relevantEffectiveness);
+    resultContainer.replaceChildren();
 
+    sortedMap.forEach((value, key) => {
 
+        let elDamageRatioContainer = document.createElement("div");
+        elDamageRatioContainer.classList.add("damage-ratio");
 
+        let heading = document.createElement("h4");
+        heading.classList.add("damage-ratio-heading")
+        heading.textContent = `  ${key} damage`;
+        elDamageRatioContainer.appendChild(heading)
+
+        value.forEach(type => {
+            let elTypeContainer = document.createElement("div");
+            elTypeContainer.classList.add("type-container");
+            elTypeContainer.style.backgroundColor = getTypeColor(type);
+            //todo german trans
+            elTypeContainer.textContent = firstCharToUpperCase(type);
+
+            elDamageRatioContainer.appendChild(elTypeContainer)
+        })
+
+        resultContainer.append(elDamageRatioContainer);
+      });
+}
+
+function getTypeColor(type) {
+
+    let typeColor = {
+        normal: "#A9AA79",
+        fire: "#F0812C",
+        water: "#6891F0",
+        grass: "#79C94F",
+        flying: "#A991F0",
+        fighting: "#C12C23",
+        poison: "#A13EA1",
+        electric: "#F8D12C",
+        ground: "#E1C168",
+        rock: "#B9A135",
+        psychic: "#F85789",
+        ice: "#AACCFF",
+        bug: "#a9b91a",
+        ghost: "#715799",
+        steel: "#B9B9D1",
+        dragon: "#7135F8",
+        dark: "#715746",
+        fairy: "#FA00C0"
+    }
+
+    return typeColor[type]
+    
+}
+
+function separateRelevantEffectiveness(input) {
+    let relevantEffectiveness = input;
+    let myMap = new Map();
+    for (const [type, damageRatio] of Object.entries(relevantEffectiveness)) {
+        if (myMap.has(damageRatio)) {
+            let typeArray = myMap.get(damageRatio)
+            typeArray.push(type);
+        } else {
+            let typeArray = [type]
+            myMap.set(damageRatio, typeArray);
+        }
+      }
+      return myMap
 }
 
 function deleteAveragedEffectiveness(input){
     let typeEffectiveness = input;
-    for (let key in typeEffectiveness) {
-        if (typeEffectiveness[key] === 1) {
-            delete typeEffectiveness[key]
+    for (let value in typeEffectiveness) {
+        if (typeEffectiveness[value] === 1) {
+            delete typeEffectiveness[value]
         }
     }
     return typeEffectiveness;
+}
+
+function firstCharToUpperCase(word) {
+    return word.at(0).toUpperCase()+word.substring(1);
 }
 
 async function getDamageRelations(firstType, secondType) {
@@ -151,7 +218,6 @@ async function getDamageRelations(firstType, secondType) {
         dragon: 1,
         dark: 1,
         fairy: 1
-    
     }
     let response = await fetch(firstType.url)
     let data = await response.json();
